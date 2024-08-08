ccef57ad04654c4e045629c660fa46ed26f01c8e
Smaragdus7 <smar.leeft@gmail.com>
2024-08-08 14:16:11 -0600

    prop children2

--- src/App.js
+++ src/App.js
@@ -28,7 +28,9 @@ export default function App() {
             <div className={step >=2? "active":""}>2</div>
             <div className={step >=3? "active":""}>3</div>
           </div>
-          <p className="message">Step {step}:{messages[step-1]}</p>
+          <StepMessage step={step}>
+            {messages[step-1]}
+          </StepMessage>
           <div className="buttons">
             <Button 
               bgColor='#7950f2'
@@ -48,6 +50,14 @@ export default function App() {
   );
 }
 
+function StepMessage(step, children){
+  return(
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
+  )
+}
 function Button({textColor, bgColor, onClick, children}) {
   return <button style={{backgroundColor: bgColor, color:textColor}} onClick={onClick}>{children}</button>
 }
